88e4fe8ca1d252f36a15c11e5ef9cb634ca914ca
Александр <oav7@outlook.com>
2020-05-24 22:52:28 +0500

    coment

--- Game/main.cpp
+++ Game/main.cpp
@@ -4,13 +4,13 @@
 #include "Headerfunction.h"
 
 using namespace std;
-																		//���������� ��������� � ����������� �������� ����
+																		
 int main() {
 	setlocale(LC_ALL, "rus");
-	system("mode con cols=70 lines=25");															//��������� ������� ����
+	system("mode con cols=70 lines=25");															
 	system("title Game");
-	system("color 0A");																				//��������� ����� ������
-	HANDLE hCons = GetStdHandle(STD_OUTPUT_HANDLE);													//��������� ����������� ����������� ����
+	system("color 0A");																				
+	HANDLE hCons = GetStdHandle(STD_OUTPUT_HANDLE);													
 	CONSOLE_CURSOR_INFO cursor = { 100,false };														
 	SetConsoleCursorInfo(hCons, &cursor);
 	startMenu(1);
